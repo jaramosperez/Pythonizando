ffa6b42a5e9b46a69a092400ef89bd35317b9c29
Javier Ramos <jaramosperez85@gmail.com>
2020-07-05 19:01:26 -0400

    Se agregó el trabajo de los indices y las técnicas de Slicing en las cadenas de caracteres.

---
+++ slicing.py
@@ -0,0 +1,12 @@
+# SLICING
+# EXTRAER TROZOS DE TEXTOS DE CADENAS
+
+palabra = 'Python'
+print(palabra[0:2])
+# COMO FUNCIONA: EL PRIMER INDICE LE DICE DESDE DONDE COMIENZA Y EL SIGUIENTE CUANTOS MOSTRARA DESDE CONTANTO EL INICIO
+print(palabra[:3])
+print(palabra[2:])
+# SI NO SE INDICA INDICE DE INICIO NI DE FIN TOMARÁ EL PRIMER Y ULTIMO INDICE
+
+# Si no se inidican indices mostrará la cadena de texto completa
+print(palabra[:])
