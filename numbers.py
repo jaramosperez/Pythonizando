aca60f6437b35d44a755d74b99ccba61bd393e08
Javier Ramos <jaramosperez85@gmail.com>
2020-07-05 12:09:14 -0400

    Subiendo el repaso de trabajo de opepraciones básicas con números.

---
+++ numbers.py
@@ -0,0 +1,17 @@
+# Suma de números.
+print('Esto es una Suma de 5 + 3 es: ', 5 + 3)
+
+# Esto es una multiplicación de números
+print('El resultado de la multiplicación es de 9 * 8 es: ', 9 * 8)
+
+# Esto es una división de 9/2
+print('Este es el resultado de la división de 9/2 es: ', 9 / 2)
+
+# Para poder conocer el resto o módulo de una división de realiza de la siguiente manera
+print('El modulo de la división de 9/2 es: ', 9 % 2)
+
+# Para trabajar la poteción de un numero se realiza de la siguiente manera.
+print('La potencia 9 elevado a 2 es: ', 9 ** 2)
+
+# Python trabaja con el orden de precedencia de las operaciones aritmeticas.
+print('El resultado de 3-2+4*10 es: ', 3 - 2 + 4 * 10)
