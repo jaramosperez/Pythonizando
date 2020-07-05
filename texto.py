ca503308e25898a430d7d43e43649652062b07d5
Javier Ramos <jaramosperez85@gmail.com>
2020-07-05 19:09:14 -0400

    Separando el trabajo con indices en otro archivo.

--- texto.py
+++ texto.py
@@ -33,27 +33,3 @@ esto es la segunda linea
 esto es \t una tabulacion
 y esta es la última linea""")
 
-
-# INDICES
-# NOS PERMITE ACCEDER A CADA CARACTER DE UN CONJUNTO DE ELLOS POR EL INDICE QUE OCUPAN
-# SE COMIENZA POR EL INDICE 0
-
-palabra = "Python"
-print("INDICES CORRELATIVOS\n")
-print(palabra[0])  # Caracter de la posición 0 corresponderia a la letra P
-print(palabra[1])  # Caracter de la posición 4 correspondería a la letra y
-print(palabra[2])  # Caracter de la posición 4 correspondería a la letra t
-print(palabra[3])  # Caracter de la posición 4 correspondería a la letra h
-print(palabra[4])  # Caracter de la posición 4 correspondería a la letra o
-print(palabra[5])  # Caracter de la posición 4 correspondería a la letra n
-
-# LOS INDICES TAMBIÉN SE PUEDE TRABAJAR CON LOS NUMEROS EN NEGATIVOS
-print("\nINDICES INVERTIDOS : \n")
-print(palabra[-1])  # Caracter de la posición 4 correspondería a la letra n
-print(palabra[-2])  # Caracter de la posición 4 correspondería a la letra o
-print(palabra[-3])  # Caracter de la posición 4 correspondería a la letra t
-print(palabra[-4])  # Caracter de la posición 4 correspondería a la letra h
-print(palabra[-5])  # Caracter de la posición 4 correspondería a la letra y
-print(palabra[-6])  # Caracter de la posición 4 correspondería a la letra p
-
-
