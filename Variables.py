292199c64153b5a2878001f198dd59a9dc554af5
Javier Ramos <jaramosperez85@gmail.com>
2020-07-05 15:59:16 -0400

    Trabajando variables numericas.

---
+++ Variables.py
@@ -0,0 +1,27 @@
+# Asignación de un valor a una variable.
+# Una variable es un espacio en memoria que almacena algún tipo de dato.
+
+print('Muestra de variables numéricas', '\n')
+primer_numero = 3
+print(primer_numero)
+segundo_numero = 39
+print(segundo_numero, '\n')  # La expresión '\n' permmite realizar un salto de linea.
+
+# Variable numéricas, se puede operar entre si.
+resultado_suma = primer_numero + segundo_numero
+resultado_resta = segundo_numero - primer_numero
+resultado_multiplicacion = primer_numero * segundo_numero
+resultado_potencia = primer_numero ** segundo_numero
+
+# Se puede mostrar los resultado almacenados en una variable
+# O se puede mostrar el resultado de la operacion sin almacenar
+
+print('Resultados  de operaciones de variables numéricas', '\n')
+print(resultado_suma)
+print(primer_numero + segundo_numero)
+print(resultado_resta)
+print(segundo_numero - primer_numero)
+print(resultado_multiplicacion)
+print(primer_numero * segundo_numero)
+print(resultado_potencia)
+print(primer_numero ** segundo_numero)
