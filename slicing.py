eabeb942e586910a2ee8cea4061e9d0f073c96b8
Javier Ramos <jaramosperez85@gmail.com>
2020-07-05 19:35:12 -0400

    Agregando ejemplo al slicing y finalizando el archivo con len.

--- slicing.py
+++ slicing.py
@@ -1,5 +1,8 @@
 # SLICING
 # EXTRAER TROZOS DE TEXTOS DE CADENAS
+# UNA CADENA DE CARACTERES EN PYTHON ES INMUTABLE
+# POR LO QUE NO SE PUEDE SUSTITUIR NINGUN DE SUS CARACTERES INDIVIDUALMENTE.
+
 
 palabra = 'Python'
 print(palabra[0:2])
@@ -15,4 +18,23 @@ print(palabra[:])
 print("\nPalabra concatenada con la técnica de slicing")
 print(palabra[:3] + palabra[3:])
 
+
+# También se puede trabajar con números negativos.
+print("\nComienzo desde una posición de un número negativo")
+print(palabra[-2:])
+
+# NO SE PUEDE TRABAJAR CON INDICES SUPERIORES A LA LONGITUD  DE LA CADENA.
+# print(palabra[99])
+
+# SE PUEDE USAR UN INDICE MAYOR PERO SOLO NOS MOSTRARÁ HASTA EL PUNTO DONDE LLEGUE LA CADENA.
+print("Usando un Indice mayor al que contiene una cadena como final!")
+print(palabra[:99])
+
+# SE PUEDEN HACER USO DE OTRAS ESTRATEGIAS PARA MODIFICAR UNA CADENA DE CARACTERES.
+print("Modificando una letra de la cadena")
+palabra = 'W' + palabra[1:]
+print(palabra)
+
+# PARA FINALIZAR SE PUEDE CONOCER LA CANTIDAD DE CARACTERES DE UNA CADENA
+print(len(palabra))
 
