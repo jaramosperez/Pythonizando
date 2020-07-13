134e9a34856e93a141d7137e82b5e1169729838b
Javier Ramos <jaramosperez85@gmail.com>
2020-07-12 21:57:50 -0400

    Agregando ejercicios 03 y 04.

---
+++ Ejercicios/Ejercicio 03.py
@@ -0,0 +1,10 @@
+# Ejercicio 3
+# El siguiente código pretende realizar una media entre 3 números, pero no funciona correctamente.
+# ¿Eres capaz de identificar el problema y solucionarlo?
+
+numero_1 = 9
+numero_2 = 3
+numero_3 = 6
+
+media = (numero_1 + numero_2 + numero_3) / 3
+print("La nota media es", media)
---
+++ Ejercicios/Ejercicio 04.py
@@ -0,0 +1,14 @@
+# Ejercicio 4
+# A partir del ejercicio anterior, vamos a suponer que cada número es una nota,
+# y lo que queremos es obtener la nota final. El problema es que cada nota tiene un valor porcentual:
+#
+# La primera nota vale un 15% del total
+# La segunda nota vale un 35% del total
+# La tercera nota vale un 50% del total
+# Desarrolla un programa para calcular perfectamente la nota final:
+nota_1 = 10
+nota_2 = 7
+nota_3 = 4
+
+nota_final = ((nota_1 * 15) / 100) + ((nota_2 * 35) / 100) + ((nota_3 * 50) / 100)
+print("La nota final es: ", nota_final)
