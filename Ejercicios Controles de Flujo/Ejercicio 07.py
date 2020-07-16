3b0285552e3415515eae008e19d4734435435533
Javier Ramos <jaramosperez85@gmail.com>
2020-07-16 18:36:43 -0400

    ordenando el codiigo

--- Ejercicios Controles de Flujo/Ejercicio 07.py
+++ Ejercicios Controles de Flujo/Ejercicio 07.py
@@ -4,15 +4,13 @@
 
 lista_1 = ["h", 'o', 'l', 'a', ' ', 'm', 'u', 'n', 'd', 'o']
 lista_2 = ["h", 'o', 'l', 'a', ' ', 'l', 'u', 'n', 'a']
-lista_3 = []
-# Completa el ejercicio aquí
-print(lista_1)
-print(lista_2)
-print(lista_1[1])
 
+# Completa el ejercicio aquí
+lista_3 = []
 for i in range(len(lista_1)):
     if lista_1[i] not in lista_3:
         if lista_1[i] in lista_2:
             lista_3.append(lista_1[i])
+
 print(lista_3)
 
