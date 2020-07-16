0a68fb0b127140ccb62fc506cf371d143ea4f89e
Javier Ramos <jaramosperez85@gmail.com>
2020-07-16 12:53:04 -0400

    Mejorando Segundo ejercicio de operadores y expresiones. Completado!

--- Ejercicios Operadores y expresiones/Ejercicio 02.py
+++ Ejercicios Operadores y expresiones/Ejercicio 02.py
@@ -3,5 +3,5 @@
 # determina si una cadena de texto introducida por el usuario
 # tiene una longitud mayor o igual que 3 y a su vez es menor que 10 (es suficiene con mostrar True o False).
 
-cadena = input('Ingresa una cita o palabra que te guste')
-print(3 < len(cadena) < 10)
+cadena = input('Ingresa una cita o palabra que te guste: ')
+print('La palabra o cita ingresada tiene mas de 3 caracteres y menos de 10? ', 3 < len(cadena) < 10)
