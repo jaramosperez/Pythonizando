4223a2294a43a41cd27b5b940e6e6521e44fba50
Javier Ramos <jaramosperez85@gmail.com>
2020-07-16 11:32:18 -0400

    Operadores relacionales.

---
+++ Conceptos/operadores_relacionales.py
@@ -0,0 +1,77 @@
+# SIRVEN PARA VERIFICAR LA AUTENTICIDAD DE UNA OPERACION O COMPARACION
+
+# IGUAL QUE
+print(3 == 2)
+# False
+
+# DISTINDO DE
+print(3 != 2)
+# True
+
+# MAYOR QUE
+print(3 > 2)
+# True
+
+# MENOR QUE
+print(3 < 2)
+# False
+
+# MAYOR O IGUAL QUE
+print(3 >= 2)
+# True
+
+# MENOR O IGUAL QUE
+print(3 <= 2)
+# False
+
+# USO DE VARIABLES
+a = 10
+b = 5
+print(a > b)
+# True
+
+print(b != a)
+# True
+
+print(a == b*2)
+# True
+
+print('Hola' == 'Hola')
+# True
+
+print('Hola' == 'hola')
+# False
+
+c = 'Hola'
+
+print(c[0] == 'H')
+# True
+
+lista1 = [0, 1, 2]
+lista2 = [2, 3, 4]
+
+print(lista1 == lista2)
+# False
+
+print(len(lista1) == len(lista2))
+# True
+print(lista1[2] == lista2[0])
+# True
+
+print(True == False)
+# False
+
+print(False != True)
+# True
+
+print(True > False)
+# True
+
+print(False < True)
+# True
+
+print(True*3)
+# 3
+
+print(False + 1)
+# 1
