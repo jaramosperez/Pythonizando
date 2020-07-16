040e8e89be316a974544ea31eb20c5dfe31443ea
Javier Ramos <jaramosperez85@gmail.com>
2020-07-16 12:30:44 -0400

    operadores de asginación!

---
+++ Conceptos/operadores_asignacion.py
@@ -0,0 +1,27 @@
+# Operaciones en asignación
+
+a = 0
+# Suma en asignación
+a += 1
+print(a)
+a += 2
+print(a)
+# Resta en asignación
+a -= 1
+print(a)
+
+# Multiplicación en asignación
+a *= 3
+print(a)
+
+# División en Asignación
+a /= 2
+a += 2
+print(a)
+
+a %= 2
+print(a)
+
+a += 2
+a **= 3
+print(a)
