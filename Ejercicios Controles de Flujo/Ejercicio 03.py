40abf36346ec1e59edc651131b39e88068a5e1ae
Javier Ramos <jaramosperez85@gmail.com>
2020-07-16 16:28:15 -0400

    Ejercicio for, range, append, sum.

---
+++ Ejercicios Controles de Flujo/Ejercicio 03.py
@@ -0,0 +1,13 @@
+# Ejercicio 3
+# Realiza un programa que sume todos los números enteros pares desde el 0 hasta el 100.
+#
+# Sugerencia
+#
+# Puedes utilizar la funciones sum() y range() para hacerlo más fácil.
+# El tercer parámetro en la función range(inicio, fin, salto) indica un salto de números, pruébalo.
+
+# UNA DE LAS FORMAS DE HACERLO
+numero = []
+for i in range(0, 102, 2):
+    numero.append(i)
+print(sum(numero))
