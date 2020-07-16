292de2d50e44baa6255f46243f1eb6a7d436dabb
Javier Ramos <jaramosperez85@gmail.com>
2020-07-16 12:51:06 -0400

    Segundo ejercicio de operadores y expresiones. Completado!

---
+++ Ejercicios Operadores y expresiones/Ejercicio 02.py
@@ -0,0 +1,7 @@
+# Ejercicio 2
+# Utilizando operadores lógicos,
+# determina si una cadena de texto introducida por el usuario
+# tiene una longitud mayor o igual que 3 y a su vez es menor que 10 (es suficiene con mostrar True o False).
+
+cadena = input('Ingresa una cita o palabra que te guste')
+print(3 < len(cadena) < 10)
