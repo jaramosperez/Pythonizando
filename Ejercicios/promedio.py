735ee6b4a474cfc13fe800638e92ad8d54889539
Javier Ramos <jaramosperez85@gmail.com>
2020-07-17 00:25:12 -0400

    calculo loco del promedio en estadistica. a la rapida, no conviene examen.

---
+++ Ejercicios/promedio.py
@@ -0,0 +1,14 @@
+nota_1 = 56
+nota_2 = 29
+nota_3 = 37
+nota_4 = 65
+
+promedio = (nota_1 + nota_2 + nota_3 + nota_4) / 4
+
+print(promedio)
+
+final = promedio * 75 / 100
+print(final)
+
+examen = 60 * 25 /100
+print(examen+final)
