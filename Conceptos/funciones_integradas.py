5088aea399769b663d580e07de9e86c756407e5c
Javier Ramos <jaramosperez85@gmail.com>
2020-07-18 14:17:43 -0400

    algunas funciones basicas integradas en python.

---
+++ Conceptos/funciones_integradas.py
@@ -0,0 +1,36 @@
+n = '10.5'
+print(n)
+float(n)
+print(n)
+
+cadena = "Un numero podría ser " + str(10) + ' y un decimal podria ser ' + str(12.4)
+print(cadena)
+
+# CONVERSION DE UN NUMERO A BINARIO
+print(bin(10))
+# CONVERSION DE UN NUMERO A HEXADECIMAL
+print(hex(13))
+
+#CONVERSION DE UNA CADENA A BINARIO
+print(int('0b1010', 2))
+#CONVERSION DE UNA CADENA A HEXADECIMAL
+print(int('0xd', 16))
+
+#VALOR ABSOLUTO
+print(abs(-10))
+print(abs(10))
+
+#REDONDEAR NUMEROS
+print(round(5.5))
+print(round(5.4))
+
+# FUNCION EVAL
+print(eval('2+6'))
+numero = 30
+print(eval('(numero *3 +15)/2'))
+
+# FUNCION LEN(largo de una variable)
+print(len('Hola soy un pythonizado'))
+print(len([]))
+
+#FUNCION HELP
