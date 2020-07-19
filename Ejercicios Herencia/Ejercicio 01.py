7d7af4c87fd9df0d00fb6ba92bfd6c2734e6a4d7
Javier Ramos <jaramosperez85@gmail.com>
2020-07-19 18:07:30 -0400

    Mejora al Ejercicio de herencia vehiculos y coches.

--- Ejercicios Herencia/Ejercicio 01.py
+++ Ejercicios Herencia/Ejercicio 01.py
@@ -28,3 +28,28 @@ class Coche(Vehiculo):
 
 c = Coche('Verde', 4, 150, '12')
 print(c)
+
+## Mejora del programa optimizando las lineas de codigo
+class Vehiculo():
+
+    def __init__(self, color, ruedas):
+        self.color = color
+        self.ruedas = ruedas
+
+    def __str__(self):
+        return "Color {}, {} ruedas".format( self.color, self.ruedas )
+
+
+class Coche(Vehiculo):
+
+    def __init__(self, color, ruedas, velocidad, cilindrada):
+        Vehiculo.__init__(self, color, ruedas)
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
+
+    def __str__(self):
+        return Vehiculo.__str__(self) + ", {} km/h, {} cc".format(self.velocidad, self.cilindrada)
+
+
+c = Coche("azul", 4, 150, 1200)
+print(c)
