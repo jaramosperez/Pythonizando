1876190eecef6e689d4573c97f87a064d8c342c1
Javier Ramos <jaramosperez85@gmail.com>
2020-07-19 17:52:36 -0400

    Herencia multiple

---
+++ Conceptos/herencia_multiple.py
@@ -0,0 +1,25 @@
+class A:
+    def __init__(self):
+        print("Soy de clase A")
+
+    def a(self):
+        print("Este método lo heredo de A")
+
+
+class B:
+    def __init__(self):
+        print("Soy de clase B")
+
+    def b(self):
+        print("Este método lo heredo de B")
+
+
+class C(B, A):
+    def c(self):
+        print("Este método es de C")
+
+
+c = C()
+c.a()
+c.b()
+c.c()
