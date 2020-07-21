e0abb629c03df736ab18dd4144797a5a0e9d8d5a
Javier Ramos <jaramosperez85@gmail.com>
2020-07-21 13:21:51 -0400

    Terminando la sección paquetes.

---
+++ Ejercicios con Modulos y paquetes/Paquetes/paquete/adios/despedidas.py
@@ -0,0 +1,8 @@
+# Este es un módulo con funciones que se despide.
+def despedirse():
+    print("Adios, me estoy despidiendo desde la función despedirse del módulo despedidas")
+
+
+class Despedida():
+    def __init__(self):
+        print("Adios, me estoy despidiendo desde el init de la clase Desdepedida")
---
+++ Ejercicios con Modulos y paquetes/Paquetes/paquete/hola/saludos.py
@@ -0,0 +1,9 @@
+# Este es un módulo con funciones que salunda.
+def saludar():
+    print("Hola, te estoy saludando desde la función saludar del módulo saludos")
+
+
+class Saludo():
+    def __init__(self):
+        print("Hola, te estoy saludando desde el init de la clase Saludo")
+
