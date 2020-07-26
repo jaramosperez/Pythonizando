63ea1d05fb9b55a652cbfc209b9899ff1988c63d
Javier Ramos <jaramosperez85@gmail.com>
2020-07-25 20:55:49 -0400

    Terminado la revisión del paquete math.

---
+++ Ejercicios con Modulos y paquetes/modulo_math.py
@@ -0,0 +1,32 @@
+# INTEGRA FUNCIONES O METODOS PARA TRABAJAR CON MATEMÁTICAS.
+import math
+
+# Redondear numeros
+pi = 3.1415920
+print(round((pi)))
+
+print(math.floor(pi))
+print(math.floor(3.55))
+print(math.floor(3.999999))
+
+print(math.ceil(pi))
+print(math.ceil(3.55))
+print(math.ceil(3.999999))
+
+print(abs(-10)) # Valor Absoluto. distancia al 0.
+
+
+n = [0.9999999, 1, 2, 3]
+print(sum(n))
+
+print(math.fsum(n)) #Fsum es mas realista y estable con los flotantes.
+
+#Truncar un número
+print(math.trunc(100.123456))
+
+print(math.pow(2, 3)) # elevar un número
+print(math.sqrt(9)) # raiz de un número
+
+print(math.pi)
+print(math.e)
+
