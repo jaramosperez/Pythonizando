5eba2b288e83eb79092c42c60f6269b418eccc31
Javier Ramos <jaramosperez85@gmail.com>
2020-07-25 22:23:51 -0400

    ordenando archivo

--- Ejercicios con Modulos y paquetes/calculos.py
+++ Ejercicios con Modulos y paquetes/calculos.py
@@ -2,7 +2,7 @@ from operaciones import *
 
 a, b, c, d = (10, 5, 0, "Hola")
 
-print( "{} + {} = {}".format(a, b, suma(a, b) ) )
-print( "{} - {} = {}".format(b, d, resta(b, d) ) )
-print( "{} * {} = {}".format(b, b, producto(b, b) ) )
-print( "{} / {} = {}".format(a, c, division(a, c) ) )
+print("{} + {} = {}".format(a, b, suma(a, b)))
+print("{} - {} = {}".format(b, d, resta(b, d)))
+print("{} * {} = {}".format(b, b, producto(b, b)))
+print("{} / {} = {}".format(a, c, division(a, c)))
