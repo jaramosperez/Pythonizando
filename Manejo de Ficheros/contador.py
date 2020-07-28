95dab73755ca58701754ebea233944a6d556e07b
Javier Ramos <jaramosperez85@gmail.com>
2020-07-27 20:41:48 -0400

    Ejercicio con fichero, aunmentar y disminuir.

---
+++ Manejo de Ficheros/contador.py
@@ -0,0 +1,32 @@
+from io import open
+import sys
+
+fichero = open('contador.txt', 'a+')
+fichero.seek(0)
+contenido = fichero.readline()
+
+if len(contenido) == 0:
+    contenido = "0"
+    fichero.write(contenido)
+
+fichero.close()
+
+try:
+    contador = int(contenido)
+
+    if len(sys.argv) == 2:
+        if sys.argv[1] == "inc":
+            contador += 1
+        elif sys.argv[1] == "dec":
+            contador -= 1
+
+    print(contador)
+
+    fichero = open("contador.txt", 'w')
+    fichero.write(str(contador))
+    fichero.close()
+except:
+    print("Error: Fichero Corrupto")
+
+
+
