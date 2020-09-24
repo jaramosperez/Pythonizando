3571a6451b383ed5fea35f84e3444fd946c6560d
Javier Ramos <javierramos@pop-os.localdomain>
2020-09-24 00:36:23 -0300

    root and frames

--- Ejercicios Tkinter/tk.py
+++ Ejercicios Tkinter/tk.py
@@ -4,8 +4,21 @@ from tkinter import *
 root = Tk()
 root.title("Hola Mundo")
 root.resizable(1, 1)
-root.iconbitmap('medical_icon.ico')
+root.iconbitmap('@hola.xbm')
 
+
+
+frame = Frame(root, width=480, height=320)
+frame.pack(fill='both', expand="1")
+frame.config(cursor="pirate")
+frame.config(bg="lightblue")
+frame.config(bd=25)
+frame.config(relief="sunken")
+
+root.config(cursor="arrow")
+root.config(bg="blue")
+root.config(bd=25)
+root.config(relief="ridge")
 
 # Comenzamos el bucle de aplicación, es como un while True
 root.mainloop() # Siempre al final
